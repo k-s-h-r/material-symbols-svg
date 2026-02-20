6390cbb44fc5fe35abcc7996fc675c46819c7363
k-s-h-r <kasahara.mail@gmail.com>
2026-02-20 21:51:00 +0900

    chore: upgrade eslint to v10 and migrate to flat config

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off',
+    },
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      '**/*.d.ts',
+      'packages/*/src/icons/**',
+      'packages/*/src/metadata/**',
+    ],
+  },
+  {
+    files: ['**/*.{js,cjs,mjs,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['scripts/**/*.js', 'scripts/**/*.cjs', '*.config.*'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+];
